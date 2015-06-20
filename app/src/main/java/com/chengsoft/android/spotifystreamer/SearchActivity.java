bfadcb4cb82a0215f0ca97107f182a57affdc753
Timothy Cheng <tim.cheng09@gmail.com>
2015-06-20 12:27:16 -0400

    - Added search widget in actionbar  - Display a toast when a search is made

--- app/src/main/java/com/chengsoft/android/spotifystreamer/SearchActivity.java
+++ app/src/main/java/com/chengsoft/android/spotifystreamer/SearchActivity.java
@@ -1,17 +1,43 @@
 package com.chengsoft.android.spotifystreamer;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
-public class SearchActivity extends ActionBarActivity {
+public class SearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_search);
+
+        // Handle all the intents
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            //use the query to search your data somehow
+            Toast toast = Toast.makeText(this, String.format("Searched for: %s", query), Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
     }
 
 
@@ -19,6 +45,19 @@ public class SearchActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_search, menu);
+
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        // New verson
+//        SearchView searchView =
+//                (SearchView) menu.findItem(R.id.action_search).getActionView();
+        SearchView searchView = (SearchView)MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+
         return true;
     }
 
